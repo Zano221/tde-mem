bbcb504708c11e8f570c06aadc1458a916ac58c2
Zano <zanopcwr@protonmail.com>
2022-11-21 14:55:30 -0300

    feat: improved RNG, added timer and better interaction/print

--- mem.cpp
+++ mem.cpp
@@ -2,9 +2,11 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <chrono>
+#include <iomanip>
 
 
-std::vector<std::string> insertIntoSequencial(std::string path) {
+std::vector<std::string> insertSAM(std::string path) {
     
     std::vector<std::string> mem;
     
@@ -19,11 +21,10 @@ std::vector<std::string> insertIntoSequencial(std::string path) {
     return mem;
 }
 
-std::vector<std::string> insertIntoRandom(std::string path) {
+std::vector<std::string> insertRAM(std::string path) {
 
     std::vector<std::string> mem;
     std::vector<std::string> auxVec;
-    std::vector<std::string>::iterator it;
 
     std::string text;
     std::ifstream file(path);
@@ -34,14 +35,15 @@ std::vector<std::string> insertIntoRandom(std::string path) {
 
     int pos = 0;
     while(auxVec.size()) {
+        srand(time(0));
         int randomPos = rand() % auxVec.size();
+        std::cout << randomPos << std::endl;
 
         mem.push_back(auxVec[randomPos]);
         auxVec.erase(auxVec.begin()+randomPos);
 
         std::string auxString = auxVec[pos];
         pos++;
-        std::cout << pos << std::endl;
     }
 
     return mem;
@@ -52,10 +54,11 @@ std::vector<std::string> insertIntoRandom(std::string path) {
 int main() {
     
     std::vector<std::string> memoria;
+    std::chrono::time_point<std::chrono::system_clock> start, end;
 
     std::string ler;
 
-    std::cout << "Insira qual para leitura: ";
+    std::cout << "Carregue um arquivo de texto: ";
     std::cin >> ler;
 
     std::string path = ".\\arquivos\\" + ler + ".txt";
@@ -64,17 +67,44 @@ int main() {
     std::cout << "Insira o modo de leitura desejado (1. Sequencial) (2. Random): ";
     std::cin >> choice;
 
+    //Inserir
     switch(choice) {
         case 1:
-            memoria = insertIntoSequencial(path); 
+            memoria = insertSAM(path); 
             break;
         case 2:
-            memoria = insertIntoRandom(path);
+            memoria = insertRAM(path);
             break;
     }
-
+    
+    std::cout << " --- LISTA --- " << std::endl;
     for(int i = 0; i < memoria.size(); i++) {
-        std::cout << memoria[i] << std::endl;
+        std::cout << i << ". " << memoria[i] << std::endl;
     }
+    
+    //Busca
+    std::cout << "Elemento a Buscar: ";
+    std::cin >> ler;
 
+    int searchPos = 0;
+    start = std::chrono::system_clock::now();
+    for(int i = 0; i < memoria.size(); i++) {
+
+        if(ler == memoria[i]) {
+            break;
+        }
+
+        searchPos++;
+    }
+    end = std::chrono::system_clock::now();
+
+    std::chrono::duration<double> tempo = end - start;
+
+    //Printar na tela
+    std::setprecision(1);
+    std::cout << std::endl << "  === [RESULTADO] === " << std::endl << std::endl;
+    std::cout << "Tipo: SAM/RAM" << std::endl; 
+    std::cout << "Posicao: " << searchPos << std::endl; 
+    std::cout << "Duracao de Pesquisa: " << tempo.count()*60*60 << " segundos" << std::endl; 
+    std::cout << "Tipo: SAM/RAM" << std::endl; 
 }
